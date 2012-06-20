fd677d76c0751a6ca548961de314fce1ef2d5a3f
Jukka-Pekka Virtanen <jukkapekka.virtanen@gmail.com>
2012-06-20 23:28:31 +0300

    Switched space-tabs to tabs.

--- channel.py
+++ channel.py
@@ -1,31 +1,31 @@
 
 
 class IrcChannel:
-    def __init__(self, name, userlist):
-        self.name = name
-        self.userlist = userlist
-        self.userlist_complete = True
-        
-    def usersMessage(self, userlist):
-        if self.userlist_complete:
-            self.userlist = userlist
-            self.userlist_complete = False
-        else:
-            self.userlist += userlist
-            
-    def usersMessageEnd(self):
-        self.userlist_complete = True
-        
-    def removeUser(self, name):
-        try:
-            self.userlist.remove(name)
-        except ValueError:
-            pass
-    
-    def addUser(self, name):
-        if not name in self.userlist:
-            self.userlist.append(name)
+	def __init__(self, name, userlist):
+		self.name = name
+		self.userlist = userlist
+		self.userlist_complete = True
+		
+	def usersMessage(self, userlist):
+		if self.userlist_complete:
+			self.userlist = userlist
+			self.userlist_complete = False
+		else:
+			self.userlist += userlist
+			
+	def usersMessageEnd(self):
+		self.userlist_complete = True
+		
+	def removeUser(self, name):
+		try:
+			self.userlist.remove(name)
+		except ValueError:
+			pass
+	
+	def addUser(self, name):
+		if not name in self.userlist:
+			self.userlist.append(name)
 
 
-        
-    
+		
+	
