98d38df1a72cb982a898a5b75d077e32a411e7c8
Jukka-Pekka Virtanen <jukkapekka.virtanen@gmail.com>
2012-06-20 23:29:45 +0300

    Refactored to smaller functions.

--- scripts/moduleloader.py
+++ scripts/moduleloader.py
@@ -3,50 +3,80 @@ from dynamicmodule import DynamicModule
 
 class ModuleLoader(BotScript):
 	def onPrivateMessage(self, source, message, fullmask):
-		sc = self.server_connection		
-		if fullmask != sc.owner:
+		# authenticating
+		if fullmask != self.server_connection.owner:
 			return
 		
+		# checking if we got a relevant message
 		try:
-			if message.startswith("!load"):
-				modulename = message.replace("!load ", "")
-				for dm in sc.dynamicmodules:
-					if dm.modulename == modulename:
-						self.PRIVMSG(source, "module " + str(dm.classvar) + " already loaded")
-						return
+			if self.tryLoad(source, message): return
 				
-				dm = DynamicModule(sc, modulename)
-				sc.dynamicmodules.append(dm)
-				
-				self.PRIVMSG(source, "loaded " + str(dm.classvar))
-				return
-				
-			if message.startswith("!reload"):
-				modulename = message.replace("!reload ", "")
-				
-				for dm in sc.dynamicmodules:
-					if dm.modulename != modulename:
-						continue
+			if self.tryReload(source, message): return
 					
-					dm.reloadModule()
-					self.PRIVMSG(source, "reloaded " + str(dm.classvar))
-					return
-				
-				self.PRIVMSG(source, "unable to find module with name " + modulename)
-				return
-					
-			if message.startswith("!unload"):
-				modulename = message.replace("!unload ", "")
-				for dm in sc.dynamicmodules:
-					if dm.modulename != modulename:
-						continue
-					
-					sc.dynamicmodules.remove(dm)
-					self.PRIVMSG(source, "unloaded  " + str(dm.classvar))
-					del dm
-					return
-				
-				self.PRIVMSG(source, "unable to find module with name " + modulename)
+			if self.tryUnload(source, message): return
 		except Exception as e:
 			self.PRIVMSG(source, "error: " + str(e))
+
+
+	def tryLoad(self, source, message):
+		if not message.startswith("!load"):
+			return False
 			
+		modulename = message.replace("!load ", "")
+		
+		# Not loading if it's already loaded
+		for dm in self.server_connection.dynamicmodules:
+			if dm.modulename == modulename:
+				self.PRIVMSG(source, "module " + str(dm.classvar) + " already loaded")
+				return True
+		
+		# Loading and appending to the list
+		dm = DynamicModule(self.server_connection, modulename)
+		self.server_connection.dynamicmodules.append(dm)
+			
+		self.PRIVMSG(source, "loaded " + str(dm.classvar))
+		return True	
+
+
+	def tryReload(self, source, message):
+		if not message.startswith("!reload"):
+			return False
+		
+		modulename = message.replace("!reload ", "")
+		
+		# finding the module to reload
+		for dm in self.server_connection.dynamicmodules:
+			if dm.modulename != modulename:
+				continue
+			
+			dm.reloadModule()
+			self.PRIVMSG(source, "reloaded " + str(dm.classvar))
+			return True
+		
+		
+		# didn't find it
+		self.PRIVMSG(source, "unable to find module with name " + modulename)
+		return True
+	
+	
+	def tryUnload(self, source, message):
+		if not message.startswith("!unload"):
+			return False
+		
+		modulename = message.replace("!unload ", "")
+		
+		# finding the module in the list by name
+		for dm in self.server_connection.dynamicmodules:
+			if dm.modulename != modulename:
+				continue
+			
+			# unloading
+			self.server_connection.dynamicmodules.remove(dm)
+			self.PRIVMSG(source, "unloaded  " + str(dm.classvar))
+			del dm
+			return True
+			
+		# module was not found
+		self.PRIVMSG(source, "unable to find module with name " + modulename)
+		return True
+	
