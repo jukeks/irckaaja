465fc60703830920e83879f56a8192a02f503759
Etunimi Sukunimi <user@example.net>
2012-07-15 16:00:10 +0300

    IrcLinkHistory now remembers line and nick

--- scripts/irclinkhistory.py
+++ scripts/irclinkhistory.py
@@ -25,11 +25,27 @@ class IrcLinkHistory(BotScript):
 		if diff < 60:
 			return "%d sekuntia" %diff
 		elif diff < 60*60:
-			return "%d minuuttia" %(diff/60)
+			minutes = diff/60.0
+			seconds = (minutes - int(minutes)) * 60
+			return "%d minuuttia, %d sekuntia" %(minutes, seconds)
 		elif diff < 60*60*24:
-			return "%.2f tuntia" %(diff/float(60*60))
+			hours = diff/(60.0*60)
+			minutes = (hours - int(hours)) * 60
+			seconds = (minutes - int(minutes)) * 60
+			return "%d tuntia, %d minuuttia, %d sekuntia" %(hours, minutes, seconds)
+		elif diff < 60*60*24*365:
+			days = diff/(60.0*60*24)
+			hours = (days - int(days)) * 24
+			minutes = (hours - int(hours)) * 60
+			seconds = (minutes - int(minutes)) * 60
+			return "%d päivää, %d tuntia, %d minuuttia, %d sekuntia" %(days, hours, minutes, seconds)
 		else:
-			return "%d päivää" %(diff/60*60*24)
+			years = diff/(60.0*60*24*365)
+			days = (years - int(years)) * 365
+			hours = (days - int(days)) * 24
+			minutes = (hours - int(hours)) * 60
+			seconds = (minutes - int(minutes)) * 60
+			return "%d vuotta, %d päivää, %d tuntia, %d minuuttia, %d sekuntia" %(years, days, hours, minutes, seconds)
 
 	def onChannelMessage(self, nick, channel_name, message, full_mask):
 		urls = re.findall(r'(https?://\S+)', message)
@@ -42,8 +58,11 @@ class IrcLinkHistory(BotScript):
 			return
 
 		for url in urls:
-			timestamp = db.get(url)
-			if not timestamp:
-				db[url] = time.time()
+			history_tuple = db.get(url)
+			if not history_tuple:
+				db[url] = (time.time(), nick, message)
+				db.sync()
 			else:
-				self.say(channel_name, "wanha! jo " + self._getDiffString(time.time(), timestamp))
+				old_time, old_nick, old_message = history_tuple
+				self.say(channel_name, "wanha! jo " + self._getDiffString(time.time(), old_time))
+				self.say(channel_name, '< ' + old_nick + '> ' + old_message + '')
